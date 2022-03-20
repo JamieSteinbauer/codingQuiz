b0f369a074aaed643ecc4fbba7156988d33f30fd
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-03-20 12:38:09 -0400

    added highscore functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,179 +1,202 @@
 //variable for each element needed for selection
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-const greetingEl = document.getElementById('greeting');
-const timerEl = document.getElementById('timer');
-const submitBtn = document.getElementById('submit');
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const greetingEl = document.getElementById("greeting");
+const timerEl = document.getElementById("timer");
+const submitBtn = document.getElementById("submit");
+const initialsEl = document.getElementById("initials");
 
 //undeclared variable allows for shuffled index
-let shuffledQuestions, currentQuestionIndex
-
-
-
-//the event listener for clicking on the start or next button
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-});
-
-//submit initials
-submitBtn.onlick = saveHighScore;
+let shuffledQuestions;
+let currentQuestionIndex = 0;
 
 //function to start game, shuffle questions randomly, and hide start button and greeting
 function startGame() {
-  startButton.classList.add('hide');
-  greetingEl.classList.add('hide');
+  startButton.classList.add("hide");
+  greetingEl.classList.add("hide");
   //start timer
   timerId = setInterval(clockTick, 1000);
   //show starting time
-  timerEl.textContent = time + ' seconds remaining';
-  shuffledQuestions = questions.sort(() => Math.random() - .5);
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide');
+  timerEl.textContent = time + " seconds remaining";
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
   setNextQuestion();
-};
+}
 
 //when next button is clicked, shuffle through questions and choose a random one
 function setNextQuestion() {
   resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
-};
+}
 
 //when function is called, fill the container with array of question, create new button element with anwers from array
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button');
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
     button.innerText = answer.text;
-    button.classList.add('btn');
+    button.classList.add("btn");
     if (answer.correct) {
-      button.dataset.correct = answer.correct
-    };
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
   });
-};
+}
 
 //reset the state of the container so that it reverts back to the original form
 function resetState() {
   clearStatusClass(document.body);
-  nextButton.classList.add('hide');
+  nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
   //TODO add something to reset time
-};
+}
 
 //use selectAnswer function to choose an answer then find out whether they should restart or continue through the array of questions
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
   setStatusClass(document.body, correct);
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
   });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
   }
-  clearInterval();
-};
-
+}
 
 //change buttons to correct/wrong color depending on array
 function setStatusClass(element, correct) {
   clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add("correct");
   } else {
-    element.classList.add('wrong')
+    element.classList.add("wrong");
   }
-};
+}
 
 //clearstatusclass function to reset button colors
 function clearStatusClass(element) {
-  element.classList.remove('correct');
-  element.classList.remove('wrong');
-};
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+// array for questions
+const questions = [
+  {
+    question: "1. What is the DOM?",
+    answers: [
+      { text: "a programming interface for web documents", correct: true },
+      { text: "a dominant element on a webpage", correct: false },
+      { text: "a series of objects listed as a string", correct: false },
+      { text: "shortened syntax that can grab a variable", correct: false },
+    ],
+  },
+  {
+    question: "2. What does a break do?",
+    answers: [
+      { text: "breaks the code", correct: false },
+      { text: "separates the different functions", correct: false },
+      { text: "attaches a line break", correct: true },
+      { text: "exits current loop", correct: false },
+    ],
+  },
+  {
+    question: "3. Arrays in JavaScript can be stored in ______.",
+    answers: [
+      { text: "number and strings", correct: false },
+      { text: "booleans", correct: false },
+      { text: "other arrays", correct: false },
+      { text: "all of the above", correct: true },
+    ],
+  },
+];
+
+// quiz state variable
+let time = questions.length * 15;
+let timerId;
 
 //timer function
 function timeRemaining() {
-    //check if user guessed wrong
-    if (this.value !== questions[currentQuestionIndex].answers) {
-        time -= 15;
+  //check if user guessed wrong
+  if (questions.value !== questions[currentQuestionIndex].answers) {
+    //penalize time
+    time -= 5;
+    console.log("points taken off");
 
-        if (time < 0) {
-            time = 0;
-        }
+    if (time < 0) {
+      time = 0;
     }
+    //display new time on page
     timerEl.textContent = time;
+  }
 }
 
 function clockTick() {
-    time--;
-    timerEl.textContent = time;
+  //update time
+  time--;
+  timerEl.textContent = time;
 
-    if (time <= 0) {
-        clearInterval(timerId);
-    }
+  //check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
 }
 
 function saveHighScore() {
-    var initials = initialsEl.value.trim();
+  //get value of input box
+  var initials = initialsEl.value.trim();
 
-    if (initials !== "") {
-        var highscores = 
-        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  //get saved scores from localStorage, if none, set to an empty array
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-        var newScore = {
-            score: time,
-            initials: initials
-        };
+    //format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
 
-        highscores.push(newScore);
-        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    //save to localStorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
-        //TODO add high scores element
-    }
+    // redirect to next page
+    window.location.href = "score.html";
+  }
 }
 
-// array for questions 
-const questions = [
-  {
-    question: 'What is the DOM?',
-        answers: [
-            { text: 'a programming interface for web documents', correct: true },
-            { text: 'a dominant element on a webpage', correct: false },
-            { text: 'a series of objects listed as a string', correct: false },
-            { text: 'shortened syntax that can grab a variable', correct: false }
-        ]
-  },
-  {
-    question: 'What does a break do?',
-        answers: [
-            { text: 'breaks the code', correct: false },
-            { text: 'separates the different functions', correct: false },
-            { text: 'attaches a line break', correct: false },
-            { text: 'exits current loop', correct: true }
-        ]
-  },
-  {
-    question: 'Arrays in JavaScript can be stored in ______.',
-    answers: [
-        { text: 'number and strings', correct: false },
-        { text: 'booleans', correct: false },
-        { text: 'other arrays', correct: false },
-        { text: 'all of the above', correct: true }
-    ]
-  }
-]
+function quizEnd() {
+  //stop timer
+  clearInterval(timerId);
 
-// quiz state variable
-var time = questions.length * 15;
-var timerId;
+  // show end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.classList.remove("hide");
+
+  // show final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  // hide questions section
+  questionContainerElement.setAttribute("class", "hide");
+}
+
+//the event listener for clicking on the start or next button
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+//submit initials
+submitBtn.addEventListener("click", saveHighScore);
